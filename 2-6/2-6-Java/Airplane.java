7b04c6f74834bb692c51061c58b1da6182cb38ab
Jakub Malhotra <jakub.malhotra@mths.ca>
2024-11-01 13:38:25 +0000

    added java code 2-6

---
+++ 2-6/2-6-Java/Airplane.java
@@ -0,0 +1,25 @@
+public class Airplane {
+    /*
+    * The speed of the plane
+    */
+    private int speed;
+
+    /**
+     * Gets the speed of the airplane.
+     * 
+     * @return the speed of the airplane
+     */
+    public int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * Sets the speed of the airplane.
+     * 
+     * @param speed the speed to set
+     */
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
+
---
+++ 2-6/2-6-Java/Jet.java
@@ -0,0 +1,18 @@
+public class Jet extends Airplane {
+
+    private static final int MULTIPLIER = 2;
+
+    public Jet() {
+        super();
+    }
+
+    public void setSpeed(int speed) {
+        super.setSpeed(speed * MULTIPLIER);
+    }
+
+    public void accelerate() {
+        super.setSpeed(getSpeed() * MULTIPLIER);
+    }
+
+}
+
