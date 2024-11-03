155348484d4eb77c733d83762d51e2ce7497c073
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-11-03 15:35:30 -0500

    fix ts ()

--- 2-6/2-6-TS/FlyTest.ts
+++ 2-6/2-6-TS/FlyTest.ts
@@ -11,22 +11,22 @@ import Jet from './Jet.ts';
 
 const biplane = new Plane();
 biplane.setSpeed(212);
-console.log(biplane.getSpeed());
+console.log(biplane.getSpeed);
 
 const boeing = new Jet();
 boeing.setSpeed(422);
-console.log(boeing.getSpeed());
+console.log(boeing.getSpeed);
 
 let x = 0;
 while (x < 4) {
     boeing.accelerate();
-    console.log(boeing.getSpeed());
-    if (boeing.getSpeed() > 5000) {
-        biplane.setSpeed(biplane.getSpeed() * 2);
+    console.log(boeing.getSpeed);
+    if (boeing.getSpeed > 5000) {
+        biplane.setSpeed(biplane.getSpeed * 2);
     } else {
         boeing.accelerate();
     }
     x++;
 }
 
-console.log(biplane.getSpeed());
+console.log(biplane.getSpeed);
