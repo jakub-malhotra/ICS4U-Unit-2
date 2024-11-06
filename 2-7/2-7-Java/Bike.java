a239120207f4972cdb66327eef324abba408affa
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-11-05 21:26:44 -0500

    fix

--- 2-7/2-7-Java/Bike.java
+++ 2-7/2-7-Java/Bike.java
@@ -1,53 +1,60 @@
 /*
  * Class representing a bike, extending the Vehicle class.
- * 
  * @version 1.0
  * @since   2024-11-05
  */
 
-/**
- * The Bike class represents a bike, which is a specific type of vehicle with
- * additional properties such as cadence and gear, and can perform actions like 
- * ringing a bell and accelerating based on cadence and gear.
- */
 public class Bike extends Vehicle {
 
     /**
      * The cadence of the bike.
+     * @type {number}
      */
-    public int cadence;
+    private int cadence = 0;
 
     /**
      * The gear of the bike.
+     * @type {number}
      */
-    public int gear;
+    private int gear = 1;
 
     /**
      * Creates a new Bike instance.
-     * 
-     * @param color     The color of the bike.
-     * @param maxSpeed  The maximum speed of the bike.
+     * @param {String} color - The color of the bike.
+     * @param {int} maxSpeed - The maximum speed of the bike.
      */
     public Bike(String color, int maxSpeed) {
         super(color, maxSpeed);
-        this.cadence = 0;
-        this.gear = 1;
     }
 
     /**
-     * Accelerates the bike by adjusting the cadence and speed based on gear.
-     * 
-     * @param appliedPower The power level for acceleration.
+     * Sets the gear of the bike.
+     * @param {int} gear - The gear to set.
+     */
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+
+    /**
+     * Sets the cadence of the bike.
+     * @param {int} cadence - The cadence to set.
+     */
+    public void setCadence(int cadence) {
+        this.cadence = cadence;
+    }
+
+    /**
+     * Accelerates the bike based on cadence and gear.
+     * @param {int} appliedPower - The power level for acceleration.
      */
     public void accelerate(int appliedPower) {
-        this.cadence += appliedPower;
-        this.currentSpeed += this.cadence * this.gear;
+        this.cadence = this.cadence + appliedPower;
+        this.currentSpeed = this.currentSpeed + (this.cadence * this.gear);
     }
 
     /**
      * Rings the bike's bell.
-     * 
-     * @return The sound of the bell.
+     * @return {String} The sound of the bell.
      */
     public String ringBell() {
         return "Ding ding!\n";
@@ -56,6 +63,7 @@ public class Bike extends Vehicle {
     /**
      * Displays the status of the bike, including cadence and gear.
      */
+    @Override
     public void status() {
         super.status();
         System.out.println("-> Cadence: " + this.cadence);
