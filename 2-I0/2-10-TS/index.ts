f62fad4e2b1ff2e72cf6d81957b74278a4835f52
Jakub Malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2024-11-14 09:20:30 -0500

    fix index.ts

--- 2-I0/2-10-TS/index.ts
+++ 2-I0/2-10-TS/index.ts
@@ -7,9 +7,9 @@
 */
 
 import Vehicle from "./Vehicle.ts"
-import { Truck } from "./Truck.ts"
-import { Bike } from "./Bike.ts"
 
 const vehicle = new Vehicle('Blue', 100, 4)
+
+console.log(`Vehicle status: ${vehicle.status}`)
 
 console.log('\nDone.')
